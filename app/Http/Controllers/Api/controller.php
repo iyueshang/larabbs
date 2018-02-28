0f08b19908f17465b4ac71b43d58e9d75510c897
iyueshang <ikaifa@qq.com>
2018-02-28 17:17:27 +0800

    添加测试接口

---
+++ app/Http/Controllers/Api/controller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Dingo\Api\Routing\Helpers;
+use App\Http\Controllers\Controller as BaseController;
+
+class controller extends BaseController
+{
+    //
+    use Helpers;
+}

c738b06ba842f490879d86c4ebb7be6c8216e97f
iyueshang <ikaifa@qq.com>
2018-03-02 13:53:31 +0800

    发送验证码

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,10 @@ use Illuminate\Http\Request;
 
 $api = app('Dingo\Api\Routing\Router');
 
-$api->version('v1',[
+$api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api'
-],function ($api){
-    $api->get('verificationCodes','VerificationCodesController@store')->name('api.VerificationCodes.store');
+], function($api) {
+    // 短信验证码
+    $api->post('verificationCodes', 'VerificationCodesController@store')
+        ->name('api.verificationCodes.store');
 });

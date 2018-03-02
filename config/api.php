dfd3ecbaccfcbe1fc3b938f0bdd3d1d995d47d23
iyueshang <ikaifa@qq.com>
2018-03-02 14:31:03 +0800

    添加 请求限制 中间件

--- config/api.php
+++ config/api.php
@@ -230,4 +230,15 @@ return [
 
     ],
 
+    'rate_limit' => [
+        'access' => [
+            'expires' => env('RATE_LIMITS_EXPIRES', 1),
+            'limit' => env('RATE_LIMITS',60)
+        ],
+        'sign' => [
+            'expires' => env('SIGN_RATE_LIMITS_EXPORES',1),
+            'limit' => env('SIGN_RATE_LIMITS',10)
+        ],
+    ],
+
 ];
